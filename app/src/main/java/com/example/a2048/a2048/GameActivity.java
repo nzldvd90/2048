3d3f50d39ab0ac60857a3f91fc2465ae4c1822b9
Davide Anzalone <davide@geckosoft.it>
2018-04-03 15:46:10 +0200

    First lesson

---
+++ app/src/main/java/com/example/a2048/a2048/GameActivity.java
@@ -0,0 +1,13 @@
+package com.example.a2048.a2048;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
