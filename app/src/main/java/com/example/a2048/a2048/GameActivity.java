161dadb0af5a3a99819ec7dea0232d1e1b416f6e
Davide Anzalone <davide@geckosoft.it>
2018-04-18 17:49:38 +0200

    app: I numeri si colorano in base al contenuto

--- app/src/main/java/com/example/a2048/a2048/GameActivity.java
+++ app/src/main/java/com/example/a2048/a2048/GameActivity.java
@@ -1,5 +1,6 @@
 package com.example.a2048.a2048;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -26,6 +27,8 @@ public class GameActivity extends AppCompatActivity {
     Griglia griglia;
 
     HashMap<Numero, TextView> listaNumeri = new HashMap<>();
+
+    HashMap<Integer, Integer> coloreNumeri = new HashMap<>();
 
     View.OnClickListener buttonListeners = new View.OnClickListener() {
         @Override
@@ -77,6 +80,8 @@ public class GameActivity extends AppCompatActivity {
 
         grigliaView = findViewById(R.id.griglia);
 
+        impostaColori();
+
         findViewById(R.id.btnSu).setOnClickListener(buttonListeners);
         findViewById(R.id.btnGiu).setOnClickListener(buttonListeners);
         findViewById(R.id.btnSinistra).setOnClickListener(buttonListeners);
@@ -94,6 +99,7 @@ public class GameActivity extends AppCompatActivity {
                 grigliaView.setLayoutParams(params);
 
                 if (griglia == null) {
+
                     griglia = new Griglia(new Griglia.Eventi() {
                         @Override
                         public void numeroCreato(Numero numero, Posizione nuovaPosizione) {
@@ -102,6 +108,8 @@ public class GameActivity extends AppCompatActivity {
 
                             // Imposta come testo della TextView il valore del numero
                             viewNumero.setText(numero.numero + "");
+
+                            viewNumero.setBackgroundColor(coloreNumeri.get(numero.numero));
 
                             // Calcola la posizione e la dimensione del nuovo numero da inserire nella griglia
                             RelativeLayout.LayoutParams paramsNumero = new RelativeLayout.LayoutParams(sizeGriglia / 4, sizeGriglia / 4);
@@ -124,7 +132,9 @@ public class GameActivity extends AppCompatActivity {
 
                         @Override
                         public void numeroRaddoppiato(Numero numero) {
-                            listaNumeri.get(numero).setText(numero.numero + "");
+                            TextView numeroRaddoppiatoView = listaNumeri.get(numero);
+                            numeroRaddoppiatoView.setText(numero.numero + "");
+                            numeroRaddoppiatoView.setBackgroundColor(coloreNumeri.get(numero.numero));
                         }
 
                         @Override
@@ -137,5 +147,19 @@ public class GameActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void impostaColori() {
+        coloreNumeri.put(2, Color.parseColor("#ffd289"));
+        coloreNumeri.put(4, Color.parseColor("#facc6b"));
+        coloreNumeri.put(8, Color.parseColor("#ffd131"));
+        coloreNumeri.put(16, Color.parseColor("#f5b82e"));
+        coloreNumeri.put(32, Color.parseColor("#f4ac32"));
+        coloreNumeri.put(64, Color.parseColor("#db8c04"));
+        coloreNumeri.put(128, Color.parseColor("#d97b29"));
+        coloreNumeri.put(256, Color.parseColor("#d9631e"));
+        coloreNumeri.put(512, Color.parseColor("#bf3415"));
+        coloreNumeri.put(1024, Color.parseColor("#a60d0d"));
+        coloreNumeri.put(2048, Color.parseColor("#a60d0d"));
     }
 }
